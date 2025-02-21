3ec2ad30c657bf4f45f889f133b0f1840e110a98
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-21 13:04:01 -0500

    add post and delete functionality

--- server.cjs
+++ server.cjs
@@ -1,12 +1,14 @@
 const { fetchCustomers } = require('./db/customers.cjs');
 const { fetchRestaurants } = require('./db/restaurants.cjs');
-const { fetchReservations } = require('./db/reservations.cjs');
+const { fetchReservations, createReservation, destroyReservation } = require('./db/reservations.cjs');
 
 const client = require('./db/client.cjs');
 client.connect();
 
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res, next) => {
   res.send('WELCOME');
@@ -36,6 +38,29 @@ app.get('/api/reservations', async(req, res, next) => {
   try {
     const allReservations = await fetchReservations();
     res.send(allReservations);
+  } catch(err) {
+    next(err);
+  }
+});
+
+app.post('/api/customers/:id/reservations', async(req, res, next) => {
+  const { id: customerId } = req.params;
+  const { restaurant_id, date, party_count } = req.body;
+  
+  try {
+    const newReservation = await createReservation(date, party_count, restaurant_id, customerId);
+    res.status(201).send(newReservation);
+  } catch(err) {
+    next(err);
+  }
+});
+
+app.delete('/api/customers/:customer_id/reservations/:id', async(req, res, next) => {
+  const { customer_id, id: reservationId } = req.params;
+  try {
+    await destroyReservation(reservationId, customer_id);
+
+    res.status(204).send({});
   } catch(err) {
     next(err);
   }
